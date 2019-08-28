d876505b9d09cb1d95f3e8e6c9a61de79190c562
Khris Kempis <khriskempis@gmail.com>
2019-08-28 13:20:19 -0700

    add then promises, need to fix

--- test/test-data.js
+++ test/test-data.js
@@ -46,25 +46,64 @@ describe("api/gigs", () => {
     });
 
     describe("Get", () => {
-      it("should return month", async () => {
-        try {
-          const monthData = await Month.create({
-            year,
-            month,
-            month_name: monthName
-          });
+      // it("should return month", async () => {
+      //   try {
+      //     const monthData = await Month.create({
+      //       year,
+      //       month,
+      //       month_name: monthName
+      //     });
 
-          const response = await chai
+      //     const response = await chai
+      //       .request(app)
+      //       .set("Content-Type", "application/json")
+      //       .get(`${GIGS_URL}/month/${monthData._id}`);
+      //     console.log(response.status);
+      //   } catch (err) {
+      //     if (err instanceof chai.AssertionError) {
+      //       throw err;
+      //     }
+      //   }
+      // });
+      it("should return month", () => {
+        return Month.create({
+          year,
+          month,
+          month_name: monthName
+        }).then(monthData => {
+          return chai
             .request(app)
-            .set("Content-Type", "application/json")
-            .get(`${GIGS_URL}/month/${monthData._id}`);
-          console.log(response.status);
-        } catch (err) {
-          if (err instanceof chai.AssertionError) {
-            throw err;
-          }
-        }
+            .get(`${GIGS_URL}/month/${monthData._id}`)
+            .then(res => {
+              expect(res).to.have.status(201);
+            });
+        });
       });
     });
   });
+  // describe("Gig route", () => {
+  //   describe("Post", () => {
+  //     it("should return gig after posting", () => {
+  //       const newGig = {
+  //         monthId: "5d43599f13ca51490c4ea11d",
+  //         days: "Fri - Sat",
+  //         dates: "7/12 - 7/13",
+  //         time: "8pm",
+  //         name: "The Last Five Years",
+  //         type: "musical",
+  //         location: "After Hours Theater",
+  //         url: "www.afterhours.com"
+  //       };
+
+  //       return chai
+  //         .request(app)
+  //         .post(`${GIGS_URL}`)
+  //         .send(newGig)
+  //         .then(res => {
+  //           console.log(res);
+  //           expect(res).to.have.status(201);
+  //         });
+  //     });
+  //   });
+  // });
 });
